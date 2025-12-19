c7ed3487e62ca8f82c10de91a99f24a6836135c9
red <pfcgm24@naver.com>
2025-12-19 18:45:55 +0900

    deploy: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/docx-cardflip'
-})
+  base: '/',
+  build: {
+    outDir: 'docs'
+  }
+});
